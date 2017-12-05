ed6de81ec7ce85b56c5b367270c851c9bc979685
Mykolas <m.bautrenas@gmail.com>
2017-12-05 09:38:59 +0200

    Basic framework

--- src/git/vavency/term_calc/Main.java
+++ src/git/vavency/term_calc/Main.java
@@ -2,8 +2,25 @@ package git.vavency.term_calc;
 
 public class Main
 {
+	private static int rez=0;
 	public static void main(String[] args) 
 	{
-		
+		if(args.length <= 0) 
+		{
+			help_print();
+		}
+		else
+		{
+			int[] i = { new Integer(args[0]), new Integer(args[2])};
+			System.out.println(new calc().call(i, args[1]));
+		}
+	}
+	
+	private static void help_print() 
+	{
+		System.out.println(" Terminal based calculator.\n Numbers and operators must be separated by spaces.");
+		System.out.println(" Special functions:\n  sqrt(n) - square root of 'n'\n"
+				+ "  pow(n,p) - multiply 'n' to the power of 'p'");
+		System.out.println(" Note: use '/'not '\\' for divide");
 	}
 }
--- src/git/vavency/term_calc/calc.java
+++ src/git/vavency/term_calc/calc.java
@@ -1,6 +1,59 @@
 package git.vavency.term_calc;
 
+import java.util.HashMap;
+
 public class calc 
 {
+	private final HashMap<String, String> charmap = new HashMap<String, String>() 
+	{
+		{
+			put("+", "add");
+			put("-", "remove");
+			put("*", "multi");
+			put("/", "divide");
+		}
+	};
 	
+	private String[] special_cases = 
+		{
+				"sqrt(",
+				"pow("
+		};
+	
+	public int rez=0;
+	
+	public int call(int[] args, String called) 
+	{
+		boolean special = false;
+		for(int i=0; i<special_cases.length;i++)
+			if(called.contains(special_cases[i]))
+				special = true; 
+				
+		if(special)
+		{
+			
+		}
+		else
+			com_call(this.charmap.get(called), args[0], args[1]);
+		return this.rez;
+	}
+	
+	
+	public void add(int i1, int i2) 
+	{
+		this.rez = i1 + i2; 
+	}
+	
+	private void com_call(String str, int i1, int i2) // Ctrl+C and Crtl+V from an old project, I knew this will come in handy.
+	{
+			try // This took the longest.
+			{
+				java.lang.reflect.Method mthd = calc.class.getMethod(str, int.class, int.class);
+				mthd.invoke(new calc(), i1, i2);
+			}
+			catch (Exception e)
+			{
+				System.out.println(e); // Give output so some sowwy lad has an idea what he did wrong.
+			}
+	}
 }
